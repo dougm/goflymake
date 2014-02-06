365b6afcfac1f230f6850026ccab873b4113c66c
Sergey Pashaev <sergey.pashaev@gmail.com>
2014-02-06 15:45:45 +0300

    Undefined variable and wrong prefix fix

--- go-flycheck.el
+++ go-flycheck.el
@@ -16,11 +16,20 @@
   (require 'go-mode)
   (require 'flycheck))
 
+(defgroup goflymake nil
+  "Support for Flymake in Go via goflymake"
+  :group 'go)
+
+(defcustom goflymake-debug nil
+  "Enable failure debugging mode in goflymake."
+  :type 'boolean
+  :group 'goflymake)
+
 (flycheck-define-checker go-goflymake
   "A Go syntax and style checker using the go utility.
 
 See URL `https://github.com/dougm/goflymake'."
-  :command ("goflymake" "-prefix=flycheck-"
+  :command ("goflymake" "-prefix=flycheck_"
             (eval (if goflymake-debug "-debug=true" "-debug=false"))
             source-inplace)
   :error-patterns ((error line-start (file-name) ":" line ": " (message) line-end))
